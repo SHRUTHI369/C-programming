27fa9cb27c62f5c84bbf215f512ca544bdd55253
SHRUTHI S <83579549+SHRUTHI369@users.noreply.github.com>
2022-11-20 11:59:14 +0530

    To perform arithmetic operations between two int

---
+++ arithmetic operations.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int n1,n2;
+	printf("Enter two integers:");
+	scanf("%d %d",&n1,&n2);
+	printf("Sum of two integers =%d\n",n1+n2);
+	printf("Difference of two integers =%d\n",n1-n2);
+	printf("Product of two integers =%d\n",n1*n2);
+	printf("Division of two integers =%d\n",n1/n2);
+	printf("Mod of two integers =%d\n",n1%n2);
+	
+	
+}
