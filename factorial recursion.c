edd24e79a5fe0b51acc6dc2813a386b6d604db05
SHRUTHI S <83579549+SHRUTHI369@users.noreply.github.com>
2022-11-25 23:27:54 +0530

    Factorial using recursion

---
+++ factorial recursion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+long int factorial(int n) {
+    if (n>=1)
+        return n*factorial(n-1);
+    else
+        return 1;
+}
+int main() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    printf("Factorial of %d = %ld", n, factorial(n));
+    return 0;
+}
